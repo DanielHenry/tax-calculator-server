36ffdd0af0f926058f99bea01668ccb826e80e09
DanielHenry <daniel.henry.39@gmail.com>
2019-02-26 02:27:34 +0700

    Add resources controller, model, and migration for Tax model

---
+++ database/migrations/2019_02_25_121847_create_taxes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taxes', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name', 255);
+            $table->tinyInteger('tax_code')->default(1);
+            $table->integer('price')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taxes');
+    }
+}
